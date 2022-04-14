e5244a426dcc3dbe619df7847f3e8431e218688b
Takafumi Watanabe <36560121+Nabefumi@users.noreply.github.com>
2022-04-13 19:39:39 -0700

    init
    init

---
+++ TwitterTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterTutorial
+//
+//  Created by Takafumi Watanabe on 2022-04-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
